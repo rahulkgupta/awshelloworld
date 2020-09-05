02603e57ac885153aae8d6623924f565f6fade1a
Rahul Gupta <rahulkartgupta@gmail.com>
2020-09-05 12:19:47 -0700

    add cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-west-2_9eRv1UGFA", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "6koenhk5rsc39ljbg3666pkc7u", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
